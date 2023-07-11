70786621812dda1f7f8497de655cec1ef33b1a28
Jacob Barber <jacob.barber@disneystreaming.com>
2023-07-11 12:45:01 -0500

    banlist tests

--- server/test/server_browser_backend/test_ban_list.py
+++ server/test/server_browser_backend/test_ban_list.py
@@ -1,15 +1,19 @@
 from server_browser_backend.ban_list import BanList
 import os
 
-def test_load_ban_list():
+def clean_up_file(file_name):   
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+def test_load_ban_list_file():
     key = "foo"
     ban_list = BanList(key, "test/resources/test_load_ban_list.txt")
 
     assert len(ban_list.get()) == 1
     assert ban_list.get().pop() == "127.0.0.1"
 
-def test_update_ban_list():
-    os.remove("test/resources/test_update_ban_list.txt")
+def test_update_ban_list_file():
+    clean_up_file("test/resources/test_update_ban_list.txt")
     key = "foo"
     ban_list = BanList(key, "test/resources/test_update_ban_list.txt")
     assert len(ban_list.get()) == 0
@@ -22,6 +26,21 @@ def test_update_ban_list():
         lines = f.readlines()
         assert len(lines) == 1
         assert lines[0].strip() == "127.0.0.1"
+
+
+def test_add_all_to_ban_list():
+    clean_up_file("test/resources/test_add_all_to_ban_list.txt")
+    key = "foo"
+    ban_list = BanList(key, "test/resources/test_add_all_to_ban_list.txt")
+    assert len(ban_list.get()) == 0
+
+    adds = ["127.0.0.1", "255.255.255.255"]
+
+    result = ban_list.add_all(key, adds)
+
+    assert result, "Failed to update ban list with key"
+    assert len(ban_list.get()) == 2
+    assert ban_list.get() == set(adds)
 
 
     
