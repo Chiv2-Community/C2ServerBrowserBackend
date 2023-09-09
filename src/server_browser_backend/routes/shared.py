b159cb4a75aa0873db3dcf9ef0f67b10e822b5f4
Jacob Barber <jacoby6000@gmail.com>
2023-09-08 20:44:56 -0500

    configurable config location

--- src/server_browser_backend/routes/shared.py
+++ src/server_browser_backend/routes/shared.py
@@ -6,17 +6,17 @@ from flask import request
 from server_browser_backend.ban_list import BanList
 from server_browser_backend.server_list import SecretKeyMissing, ServerList
 
-ADMIN_KEY_HEADER = "x-chiv2-server-browser-admin-key"
 ADMIN_KEY = os.environ.get("ADMIN_KEY", secrets.token_urlsafe(128))
+CONFIG_DIR = os.environ.get("CONFIG_DIR", "config")
 
+ADMIN_KEY_HEADER = "x-chiv2-server-browser-admin-key"
 KEY_HEADER = "x-chiv2-server-browser-key"
-
 
 heartbeat_timeout = 65
 server_list: ServerList = ServerList(heartbeat_timeout)
 
-ban_list: BanList = BanList(ADMIN_KEY, "ban_list.txt")
-
+ban_list: IpList = IpList(ADMIN_KEY, f"{CONFIG_DIR}/ban_list.txt")
+allow_list: IpList = IpList(ADMIN_KEY, f"{CONFIG_DIR}/allow_list.txt")
 
 class Banned(Exception):
     pass
