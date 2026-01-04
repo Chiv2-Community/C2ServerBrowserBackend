2e8f402e6aec2c8443596bafd7655bf1c9e3b790
Jacob Barber <jacoby6000@gmail.com>
2026-01-03 23:13:41 -0600

    Update scripts, bump version

---
+++ ci.ps1
@@ -0,0 +1,4 @@
+﻿poetry install --no-root
+poetry run mypy src
+$env:DEBUG_MODE = "true"
+poetry run pytest
---
+++ publish.ps1
@@ -0,0 +1,14 @@
+﻿# If we can't login, no reason to continue.
+docker login
+
+$version = Select-String -Path "pyproject.toml" -Pattern "version" |
+        Select-Object -First 1 |
+        ForEach-Object { $_.Line -replace 'version = ', '' -replace '"', '' -replace "'", '' } |
+        ForEach-Object { $_.Trim() }
+
+./ci.ps1
+
+docker buildx build . `
+  -t "jacoby6000/chivalry2-unofficial-server-browser-backend:$version" `
+  -t "jacoby6000/chivalry2-unofficial-server-browser-backend:latest" `
+  --push
